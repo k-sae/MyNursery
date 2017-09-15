d97163070a1a6da25c3cdb2e8dd95616098dafd6
amr-sameh <amrsameh.develop@gmail.com>
2017-09-15 12:55:50 +0200

    add nursery activity

--- app/src/main/java/com/kareem/mynursery/AddNursery.java
+++ app/src/main/java/com/kareem/mynursery/AddNursery.java
@@ -3,6 +3,7 @@ package com.kareem.mynursery;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -30,13 +31,14 @@ public class AddNursery extends AppCompatActivity {
              phone1 , phone2 , facebook , instagram , snapchat , price,
              minAge , maxAge , additionalActivities ;
 
-    CheckBox swimming , drawing, football ;
+    CheckBox swimming , drawing, football , disabilites;
 
     String nurseryNameData ,nurseryDescriptionData , startTimeData , endTimeData,
             phone1Data , phone2Data , facebookData , instagramData , snapchatData ,
              additionalActivitiesData ;
     double  priceData;
     int minAgeData , maxAgeData;
+    boolean disabilitesData = false;
 
 
     @Override
@@ -49,9 +51,15 @@ public class AddNursery extends AppCompatActivity {
             editRender();//TODO fill the form with the nursery data
         else
             render();
-        String s = nurseryName.getText().toString();
-        Toast toast =  Toast.makeText(this,s,Toast.LENGTH_LONG);
-        toast.show();
+
+
+        addNursery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                featchData();
+                parseToNursery();
+            }
+        });
     }
 
     public void editRender(){
@@ -83,9 +91,14 @@ public class AddNursery extends AppCompatActivity {
         swimming = (CheckBox) findViewById(R.id.addNursery_activity_swim);
         football =(CheckBox) findViewById(R.id.addNursery_activity_football);
         drawing = (CheckBox) findViewById(R.id.addNursery_activity_draw);
+        disabilites=(CheckBox) findViewById(R.id.addNurseryDisability);
     }
 
     private void featchData(){
+        pickedImagesPaht = new ArrayList<String>();
+        activities = new ArrayList<String>();
+
+
         nurseryNameData = nurseryName.getText().toString();
         nurseryDescriptionData = nurseryDescription.getText().toString();
         startTimeData = startTime.getText().toString();
@@ -100,12 +113,39 @@ public class AddNursery extends AppCompatActivity {
         maxAgeData = Integer.parseInt(maxAge.getText().toString());
         additionalActivitiesData = additionalActivities.getText().toString();
 
+        if (swimming.isChecked())
+            activities.add("SWIMMING");
+        if (football.isChecked())
+            activities.add("FOOTBALL");
+        if (drawing.isChecked())
+            activities.add("DRAWING");
+        if (additionalActivitiesData!=null && additionalActivitiesData!="")
+            activities.add(additionalActivitiesData);
+        if (disabilites.isChecked())
+            disabilitesData=true;
+
+
     }
 
     public void parseToNursery(){
         nurseryObj = new Nursery();
-        nurseryObj
+        nurseryObj.setName(nurseryNameData);
+        nurseryObj.setDescription(nurseryDescriptionData);
+        nurseryObj.setStartTime(startTimeData);
+        nurseryObj.setEndTime(endTimeData);
+        nurseryObj.setPhone1(phone1Data);
+        nurseryObj.setPhone2(phone2Data);
+        nurseryObj.setFacebook(facebookData);
+        nurseryObj.setInstagram(instagramData);
+        nurseryObj.setSnapchat(snapchatData);
+        nurseryObj.setExpenses(priceData);
+        nurseryObj.setMinAge(minAgeData);
+        nurseryObj.setMaxAge(maxAgeData);
+        nurseryObj.setActivities(activities);
+        nurseryObj.setSupportingDisablilites(disabilitesData);
     }
+
+
 
 
 
--- app/src/main/java/com/kareem/mynursery/model/Nursery.java
+++ app/src/main/java/com/kareem/mynursery/model/Nursery.java
@@ -43,4 +43,213 @@ public class Nursery {
     //custom access info
     private boolean isSponsored;
     private String sponsershipEndDate;
+
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getGovenment() {
+        return govenment;
+    }
+
+    public void setGovenment(String govenment) {
+        this.govenment = govenment;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getMoreDetails() {
+        return moreDetails;
+    }
+
+    public void setMoreDetails(String moreDetails) {
+        this.moreDetails = moreDetails;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getSnapchat() {
+        return snapchat;
+    }
+
+    public void setSnapchat(String snapchat) {
+        this.snapchat = snapchat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(double expenses) {
+        this.expenses = expenses;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean isSupportingDisablilites() {
+        return isSupportingDisablilites;
+    }
+
+    public void setSupportingDisablilites(boolean supportingDisablilites) {
+        isSupportingDisablilites = supportingDisablilites;
+    }
+
+    public ArrayList<String> getImagesId() {
+        return imagesId;
+    }
+
+    public void setImagesId(ArrayList<String> imagesId) {
+        this.imagesId = imagesId;
+    }
+
+    public ArrayList<String> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(ArrayList<String> activities) {
+        this.activities = activities;
+    }
+
+    public boolean isSponsored() {
+        return isSponsored;
+    }
+
+    public void setSponsored(boolean sponsored) {
+        isSponsored = sponsored;
+    }
+
+    public String getSponsershipEndDate() {
+        return sponsershipEndDate;
+    }
+
+    public void setSponsershipEndDate(String sponsershipEndDate) {
+        this.sponsershipEndDate = sponsershipEndDate;
+    }
 }
