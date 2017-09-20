39c694357a493c74e5d3eef59145048c15bcc593
kareem <karim.salah2048@gmail.com>
2017-09-20 20:29:06 +0200

    add basic layout for list fragment

--- app/src/main/java/com/kareem/mynursery/nursery/NurseryListActivity.java
+++ app/src/main/java/com/kareem/mynursery/nursery/NurseryListActivity.java
@@ -17,15 +17,6 @@ public class NurseryListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nursery_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
