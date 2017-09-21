c5208029bf727859bfb5829766260e60c3799203
kareem <karim.salah2048@gmail.com>
2017-09-21 15:10:10 +0200

    add glideSliderView for better performance

---
+++ app/src/main/java/com/kareem/mynursery/GlideSliderView.java
@@ -0,0 +1,28 @@
+package com.kareem.mynursery;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+
+/**
+ * Created by kareem on 9/21/17.
+ */
+
+public class GlideSliderView extends DefaultSliderView {
+    public GlideSliderView(Context context) {
+        super(context);
+    }
+    protected void bindEventAndShow(final View v, ImageView targetImageView) {
+        View progressBar = v.findViewById(com.daimajia.slider.library.R.id.loading_bar);
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+        Glide.with(getContext())
+                .load(getUrl())
+                .into(targetImageView);
+    }
+}
