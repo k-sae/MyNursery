575d5ee35db3baa9aff13003e207230532af2d12
kareem <karim.salah2048@gmail.com>
2017-09-22 23:47:27 +0200

    calculate the distance for the user to the nursery

--- app/src/main/java/com/kareem/mynursery/Utils.java
+++ app/src/main/java/com/kareem/mynursery/Utils.java
@@ -1,6 +1,7 @@
 package com.kareem.mynursery;
 
 import android.content.Context;
+import android.location.Location;
 import android.widget.Toast;
 
 /**
@@ -11,5 +12,9 @@ public class Utils {
     public static void showToast(String text, Context context)
     {
         Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+    public static double calculateDistance(Location locationA,Location locationB )
+    {
+       return locationA.distanceTo(locationB) ;
     }
 }
